54fbac22cca973f7d2fab78133b68ab329c6c233
sujit1266 <122362384+sujit1266@users.noreply.github.com>
2025-02-12 23:58:56 +0530

    Add files via upload

---
+++ nextly-template-main/src/components/data.js
@@ -0,0 +1,60 @@
+import {
+  FaceSmileIcon,
+  ChartBarSquareIcon,
+  CursorArrowRaysIcon,
+  DevicePhoneMobileIcon,
+  AdjustmentsHorizontalIcon,
+  SunIcon,
+} from "@heroicons/react/24/solid";
+
+import benefitOneImg from "../../public/img/benefit-one.png";
+import benefitTwoImg from "../../public/img/benefit-two.png";
+
+const benefitOne = {
+  title: "About Us",
+  desc: "At marketianhub, we believe in delivering more than just marketing services. We craft stories, build connections, and create experiences that help your brand stand out in a competitive market.As a premier digital marketing agency in Kolkata, our team comprises experienced strategists, creative designers, data- driven analysts, and forward-thinking developers who work in synergy to deliver results that matter. Whether you're a local business looking to expand your reach or a global enterprise aiming to strengthen your digital footprint, our comprehensive solutions are tailored to meet your unique needs.",
+image: benefitOneImg,
+  bullets: [
+    {
+      title: "Understand your customers",
+      desc: "Then explain the first point breifly in one or two lines.",
+      icon: <FaceSmileIcon />,
+    },
+    {
+      title: "Improve acquisition",
+      desc: "Here you can add the next benefit point.",
+      icon: <ChartBarSquareIcon />,
+    },
+    {
+      title: "Drive customer retention",
+      desc: "This will be your last bullet point in this section.",
+      icon: <CursorArrowRaysIcon />,
+    },
+  ],
+};
+
+const benefitTwo = {
+  title: "Offer more benefits here",
+  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+  image: benefitTwoImg,
+  bullets: [
+    {
+      title: "Mobile Responsive Template",
+      desc: "Nextly is designed as a mobile first responsive template.",
+      icon: <DevicePhoneMobileIcon />,
+    },
+    {
+      title: "Powered by Next.js & TailwindCSS",
+      desc: "This template is powered by latest technologies and tools.",
+      icon: <AdjustmentsHorizontalIcon />,
+    },
+    {
+      title: "Dark & Light Mode",
+      desc: "Nextly comes with a zero-config light & dark mode. ",
+      icon: <SunIcon />,
+    },
+  ],
+};
+
+
+export { benefitOne, benefitTwo };
